3efe93ac760c0b13dd035a02a25686f49f1a5f63
Tiago Oliveira <contato@otao.dev.br>
2024-01-06 19:12:55 -0300

    feat: anemic entity examaple

---
+++ src/Entities/Customer.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Otaodev\Ddd\Entities;
+
+class Customer
+{
+    public function __construct(
+        private string $id,
+        private string $name,
+        private string $address,
+    )
+    { }
+
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    public function setId(string $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getAddress(): string
+    {
+        return $this->address;
+    }
+
+    public function setAddress(string $address): self
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+}
