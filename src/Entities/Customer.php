899fa79265e6cc11b9a4d08370654ae755e1f585
Tiago Oliveira <contato@otao.dev.br>
2024-01-06 19:56:40 -0300

    refactor: autovalidation principle examaple

--- src/Entities/Customer.php
+++ src/Entities/Customer.php
@@ -2,25 +2,41 @@
 
 namespace Otaodev\Ddd\Entities;
 
+use DomainException;
+use LengthException;
+
 class Customer
 {
     public function __construct(
         private string $id,
         private string $name,
         private string $address,
-        private bool $active = true
+        private bool $active = false
     )
-    { }
+    { 
+        $this->validate();
+    }
+
+    public function validate(): void
+    {
+        if (strlen($this->name) === 0) {
+            throw new LengthException("Name is required");
+        }
+    }
 
     public function changeName(string $name): self
     {
         $this->name = $name;
-
+        $this->validate();
         return $this;
     }
 
     public function activate(): self
     {
+        if (empty($this->address)) {
+            throw new DomainException("Address is mandatory to activate a customer");
+        }
+
         $this->active = true;
         return $this;
     }
