10c08242a68fa2d23de52f3df565bfa58f309eb2
Tiago Oliveira <contato@otao.dev.br>
2024-01-07 17:33:44 -0300

    test: relating item with product

--- tests/Unit/Entities/OrderTest.php
+++ tests/Unit/Entities/OrderTest.php
@@ -13,23 +13,32 @@ describe("Order unit tests", function () {
       $order = new Order('any_id', '', []);
    })->throws(Exception::class, "Customer id is required");
 
-   it("should throw an error when item quantity is equal zero", function() {
-      $order = new Order('any_id', 'any_customer_id', []);
-   })->throws(Exception::class, "Item quantity must be greater than zero");
-
    it("should calculate total", function() {
-      $item1 = new OrderItem('1', 'item 1', 10.0, 1);
-      $item2 = new OrderItem('2', 'item 2', 40.0, 1);
+      $item1 = new OrderItem('1', 'item 1', 10.0, 'any_product_id', 2);
+      $item2 = new OrderItem('2', 'item 2', 40.0, 'any_product_id', 2);
 
       $order = new Order('any_id', 'any_customer_id', [$item1]);
       $order->total();
 
-      expect($order->getTotal())->toBe(10.0);
+      expect($order->getTotal())->toBe(20.0);
 
       $order2 = new Order('any_id', 'any_customer_id', [$item1, $item2]);
       $order2->total();
 
-      expect($order2->getTotal())->toBe(50.0);
+      expect($order2->getTotal())->toBe(100.0);
    });
+
+   it("should throw an error when order quantity is equal zero", function() {
+      $order = new Order('any_id', 'any_customer_id', []);
+   })->throws(Exception::class, "Order must have at least one item");
+
+   it("should throw error if the item is less or equal zero", function() {
+      $item1 = new OrderItem('1', 'item 1', 10.0, 'any_product_id',0);
+
+      $order = new Order('any_id', 'any_customer_id', [$item1]);
+
+      $order->total();
+
+   })->throws(Exception::class, "Item quantity must be greater than zero");
 
 });
