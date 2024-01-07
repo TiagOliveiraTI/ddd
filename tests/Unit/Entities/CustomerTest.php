a893d83be36ec015403e872844f216562f9d20e4
Tiago Oliveira <contato@otao.dev.br>
2024-01-07 12:16:21 -0300

    test: ensure correct behaviors in Customer

---
+++ tests/Unit/Entities/CustomerTest.php
@@ -0,0 +1,50 @@
+<?php
+
+use Otaodev\Ddd\Entities\Customer;
+use Otaodev\Ddd\ValueObjects\Address;
+
+describe("Customer unit tests", function () {
+
+   it('should throw error when id is empty', function () {
+      $customer = new Customer("", "any_name");
+   })->throws(Exception::class, "Id is required");
+
+   it('should throw error when name is empty', function () {
+      $customer = new Customer("any_id", "");
+   })->throws(Exception::class, "Name is required");
+
+   it('should change name', function () {
+      $customer = new Customer("any_id", "any_name");
+
+      $customer->changeName("new_name");
+
+      expect($customer->getName())->toBe('new_name');
+   });
+
+   it('should throws when address is undefined', function () {
+      $customer = new Customer("any_id", "any_name");
+
+      $customer->activate();
+
+   })->throws(Exception::class, "Address is mandatory to activate a customer");
+
+   it('should activate a customer', function () {
+      $customer = new Customer("any_id", "any_name");
+      $address = new Address("any_street", "1", "any_zip", "any_city");
+
+      $customer->setAddress($address);
+      $customer->activate();
+
+      expect($customer->isActive())->toBeTrue();
+
+   });
+
+   it('should deactivate a customer', function () {
+      $customer = new Customer("any_id", "any_name");
+
+      $customer->deactivate();
+
+      expect($customer->isActive())->toBeFalse();
+
+   });
+});
