e8ec13bedaff0a3df81431419c155139e98558b2
Tiago Oliveira <contato@otao.dev.br>
2024-01-06 21:21:27 -0300

    feat: add a value object

--- src/Entities/Customer.php
+++ src/Entities/Customer.php
@@ -1,36 +1,33 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Otaodev\Ddd\Entities;
 
 use DomainException;
 use LengthException;
+use Otaodev\Ddd\Entities\Address;
 
 class Customer
 {
     public function __construct(
         private string $id,
         private string $name,
-        private string $address,
+        private Address $address,
         private bool $active = false
     )
     { 
         $this->validate();
     }
 
-    public function validate(): void
-    {
-        if (strlen($this->name) === 0) {
-            throw new LengthException("Name is required");
-        }
-    }
-
+    
     public function changeName(string $name): self
     {
         $this->name = $name;
         $this->validate();
         return $this;
     }
-
+    
     public function activate(): self
     {
         if (empty($this->address)) {
@@ -40,10 +37,33 @@ class Customer
         $this->active = true;
         return $this;
     }
-
+    
     public function deactivate(): self
     {
         $this->active = false;
         return $this;
+    }
+
+    public function setAdress(Address $address): self
+    {
+        $this->address = $address;
+        return $this;
+    }
+
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    public function isActive(): bool
+    {
+        return $this->active;
+    }
+        
+    private function validate(): void
+    {
+        if (strlen($this->name) === 0) {
+            throw new LengthException("Name is required");
+        }
     }
 }
