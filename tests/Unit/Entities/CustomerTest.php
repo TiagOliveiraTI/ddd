7bca031753e873518043a884903708e5b801a296
Tiago Oliveira <contato@otao.dev.br>
2024-01-26 23:17:52 -0300

    test: ensure Customer reward points sum is correct

--- tests/Unit/Entities/CustomerTest.php
+++ tests/Unit/Entities/CustomerTest.php
@@ -47,4 +47,18 @@ describe("Customer unit tests", function () {
       expect($customer->isActive())->toBeFalse();
 
    });
+
+   it("should add reward points", function () {
+      $customer = new Customer("any_id", "any_name");
+
+      expect($customer->getRewardPoints())->tobe(0.0);
+
+      $customer->addRewardPoints(10.0);
+
+      expect($customer->getRewardPoints())->tobe(10.0);
+
+      $customer->addRewardPoints(10.0);
+
+      expect($customer->getRewardPoints())->tobe(20.0);
+   });
 });
